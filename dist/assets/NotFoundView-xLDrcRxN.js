08394fcfffbba6938e5a9652c6a126882ae4d32e
AlvaroCodes <aramirezm098@gmail.com>
2024-01-15 17:28:29 +0100

    js dist

---
+++ dist/assets/NotFoundView-xLDrcRxN.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-7tS_8M4f.js";const o={},s={class:"text-4xl mt-10 text-red-600"};function n(r,_){return c(),t("h1",s,"404")}const d=e(o,[["render",n]]);export{d as default};
