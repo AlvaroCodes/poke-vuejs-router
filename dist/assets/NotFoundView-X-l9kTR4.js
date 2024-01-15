6dcf38a1de6133e52e136f08baae247be862d7d2
AlvaroCodes <aramirezm098@gmail.com>
2024-01-15 17:25:03 +0100

    dist update vite.config

--- dist/assets/NotFoundView-X-l9kTR4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./index-FH2QcRR6.js";const o={},s={class:"text-4xl mt-10 text-red-600"};function n(r,_){return c(),t("h1",s,"404")}const d=e(o,[["render",n]]);export{d as default};
